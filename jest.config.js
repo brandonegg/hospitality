f89fd631555dc5c8d4917a72b718083c21102e09
Kevin Dao <kevin-dao@uiowa.edu>
2023-02-26 00:53:23 -0600

    Add rest of cases for sign up jest test

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js", "<rootDir>/singleton.ts"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   modulePathIgnorePatterns: ["./tests/e2e/*"],
   moduleNameMapper: {
     // Handle module aliases (this will be automatically configured for you soon)
--- tests/jest/signup.spec.ts
+++ tests/jest/signup.spec.ts
@@ -1,17 +1,89 @@
 import { describe, expect, test } from "@jest/globals";
 import type { PrismaClient, User } from "@prisma/client";
-import { type inferProcedureInput } from "@trpc/server";
+import { type inferProcedureInput, TRPCError } from "@trpc/server";
 import { mockDeep } from "jest-mock-extended";
 
 import { appRouter, type AppRouter } from "../../src/server/api/root";
 import type { RouterOutputs } from "../../src/utils/api";
 
+const mockUser: User | null = {
+  id: "test-id",
+  name: "test user",
+  username: "test-username",
+  email: "test-user@example.com",
+  dateOfBirth: new Date("1990-01-01"),
+  password: "password",
+  phoneNumber: null,
+  emailVerified: null,
+  image: null,
+};
+
+const emptyUser: User | null = null;
+
 describe("sign up router", () => {
-  test("example router", async () => {
+  test("rejects if password and confirm password do not match", async () => {
     // mock the prisma client db
     const prismaMock = mockDeep<PrismaClient>();
 
-    const mockUser: User | null = null;
+    const caller = appRouter.createCaller({
+      session: null,
+      prisma: prismaMock,
+    });
+
+    type Input = inferProcedureInput<AppRouter["user"]["signup"]>;
+    const input: Input = {
+      firstName: "test",
+      lastName: "user",
+      dateOfBirth: "1990-01-01",
+      username: "test-username",
+      email: "test-user@example.com",
+      password: "password1234",
+      confirmPassword: "password",
+    };
+
+    await expect(caller.user.signup(input)).rejects.toBeInstanceOf(TRPCError);
+    await expect(caller.user.signup(input)).rejects.toThrow(
+      new TRPCError({
+        code: "BAD_REQUEST",
+        message: "Passwords do not match",
+      })
+    );
+  });
+
+  test('rejects if "username" is already taken', async () => {
+    // mock the prisma client db
+    const prismaMock = mockDeep<PrismaClient>();
+
+    const caller = appRouter.createCaller({
+      session: null,
+      prisma: prismaMock,
+    });
+
+    type Input = inferProcedureInput<AppRouter["user"]["signup"]>;
+    const input: Input = {
+      firstName: "test",
+      lastName: "user",
+      dateOfBirth: "1990-01-01",
+      username: "test-username",
+      email: "test-user@example.com",
+      password: "password",
+      confirmPassword: "password",
+    };
+
+    prismaMock.user.findFirst.mockResolvedValue(mockUser);
+
+    await expect(caller.user.signup(input)).rejects.toBeInstanceOf(TRPCError);
+    await expect(caller.user.signup(input)).rejects.toThrow(
+      new TRPCError({
+        code: "BAD_REQUEST",
+        message: "Username is already taken",
+      })
+    );
+  });
+
+  test("successfully create user", async () => {
+    // mock the prisma client db
+    const prismaMock = mockDeep<PrismaClient>();
 
     const mockOutput: RouterOutputs["user"]["signup"] = {
       id: "test-id",
@@ -36,7 +108,7 @@ describe("sign up router", () => {
       confirmPassword: "password",
     };
 
-    prismaMock.user.findFirst.mockResolvedValue(mockUser);
+    prismaMock.user.findFirst.mockResolvedValue(emptyUser);
     prismaMock.user.create.mockResolvedValue(mockOutput as User);
 
     const result = await caller.user.signup(input);
