38101ec690045880d04c4d3ec8161187899b5935
Brandon Egger <brandonegger64@gmail.com>
2023-03-06 17:25:22 -0600

    switch to heroicon for svg

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,6 +1,7 @@
 /**
  * Navigation bar component displayed at the top of page for convenience navigating to various site resources and pages
  */
+import { HomeIcon } from '@heroicons/react/24/solid';
 import Link from 'next/link';
 import type { ReactNode } from 'react';
 
@@ -15,16 +16,16 @@ interface NavLinkProps {
 const PagesConfig: NavLinkProps[] = [
     {
         // TODO: Change this to 'import' the hero icon properly
-        label: <>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-            </svg>
-        </>,
+        label: <HomeIcon className='h-6 w-6'/>,
         href: '/'
     },
     {
         label: 'Dashboard',
         href: '/dashboard'
+    },
+    {
+        label: 'Support',
+        href: '/support'
     }
 ]
 
