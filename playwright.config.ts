25e9749272cf886ac302603f6848511d30adc55a
Brandon Egger <brandonegger64@gmail.com>
2023-03-08 11:50:11 -0600

    mend

--- playwright.config.ts
+++ playwright.config.ts
@@ -61,7 +61,7 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-/*
+
     {
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
@@ -70,7 +70,7 @@ export default defineConfig({
     {
       name: "webkit",
       use: { ...devices["Desktop Safari"] },
-    },*/
+    },
 
     /* Test against mobile viewports. */
     // {
