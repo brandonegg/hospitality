4b5fef3b9f884a0ecd3935fc52524b8e591cde48
Brandon Egger <brandonegger64@gmail.com>
2023-03-08 02:50:09 -0600

    add test for not logged in

--- tests/e2e/home.spec.ts
+++ tests/e2e/home.spec.ts
@@ -16,13 +16,32 @@ test.describe('homepage', () => {
 });
 
 test.describe('navbar', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+  });
+
   test('shows login and signup when no user signed in', async ({page}) => {
     await expect(page.getByRole('link', { name: 'Login' })).toBeVisible();
     await expect(page.getByRole('link', { name: 'Sign up' })).toBeVisible();
   });
 
+  /*
   test('shows Dashboard link when user signed in', async ({page}) => {
-    await expect(page.getByRole('link', { name: 'Login' })).toBeVisible();
-    await expect(page.getByRole('link', { name: 'Sign up' })).toBeVisible();
+    const mockSession: Session = {
+      expires: "1",
+      user: { 
+        id: "1", 
+        username: "e2e", 
+        name: "e2e" 
+      },
+    };
+
+    jest.mock('next-auth/react', () => ({
+      useSession: jest.fn().mockReturnValue(mockSession),
+    }));
+  
+    await expect(page.getByRole('link', { name: 'Login' })).toBeHidden();
+    await expect(page.getByRole('link', { name: 'Sign up' })).toBeHidden();
   });
+  */
 });
