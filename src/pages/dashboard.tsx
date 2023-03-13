fd0c75d0d1e3948964474edc0d2ad352ef0eebb9
Brandon Egger <brandonegger64@gmail.com>
2023-03-12 20:07:33 -0500

    add basic dashboard layout

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,47 @@
 import { HeartIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import type { NextPage } from "next/types";
 import { useSession } from "next-auth/react";
 
 import NavigationBar from "../components/NavigationBar";
+
+interface SquareWidgetProperties {
+    width: 1 | 2; // Width of the widget relative to grid cells.
+    children: JSX.Element;
+}
+
+/**
+ * A square widget element for displaying useful information in the dashboard
+ * @param param0 
+ * @returns 
+ */
+const SquareWidget = ({width, children}: SquareWidgetProperties) => {
+    return <>
+        <div className={"aspect-square border overflow-hidden border-gray-600 rounded-xl drop-shadow-lg " + (width == 2 ? 'col-span-2' : 'col-span-1')}>
+            {children}
+        </div>
+    </>
+};
+
+interface DashBoardNavButtonProperties {
+    href: string;
+    label: string;
+}
+
+/**
+ * Dashboard nav button component. Shown below the main nav bar
+ * @param param0 
+ */
+const DashBoardNavButton = ({href, label}: DashBoardNavButtonProperties) => {
+    return <>
+        <Link href={href} className="bg-sky-200 h-full w-28 border border-gray-600 rounded-lg">
+            <div className="grid place-content-center h-full w-full text-center">
+                <span>{label}</span>
+            </div>
+        </Link>
+    </>
+}
 
 /**
  * User dashboard page
@@ -18,7 +56,6 @@ const Dashboard: NextPage = () => {
         },
     });
 
-    // TODO: add loading animation until authentication has been received
     if (status === 'loading') {
         return <></>;
     }
@@ -31,7 +68,29 @@ const Dashboard: NextPage = () => {
             </h2>
             <NavigationBar session={sessionData}/>
         </div>
-        <p>Temporary</p>
+        <div className="m-6 gap-4">
+            <section className="px-2 m-8 border-b-2 pb-2">
+                <div className="flex justify-between">
+                    <h2 className="font-bold text-3xl">Welcome, {sessionData.user.name}</h2>
+                    <div className="flex space-x-4">
+                        <DashBoardNavButton label="Appointments" href="/"/>
+                        <DashBoardNavButton label="Documents" href="/"/>
+                        <DashBoardNavButton label="Refills" href="/"/>
+                    </div>
+                </div>
+            </section>
+
+            <div className="m-8 grid grid-cols-6 gap-8">
+                <SquareWidget width={2}>
+                    <p>Weather here</p>
+                </SquareWidget>
+                <SquareWidget width={2}>
+                    <div className="p-4 bg-yellow-100 h-full">
+                        <h1 className="font-bold text-xl ">Upcomming Appointments:</h1>
+                    </div>
+                </SquareWidget>
+            </div>
+        </div>
     </main>
     </>
 };
