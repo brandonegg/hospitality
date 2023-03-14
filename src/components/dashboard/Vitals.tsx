996d71e8e549949a576c130dacec01068ec379e7
Brandon Egger <brandonegger64@gmail.com>
2023-03-14 18:45:00 -0500

    improve vitals contrast

--- src/components/dashboard/Vitals.tsx
+++ src/components/dashboard/Vitals.tsx
@@ -54,7 +54,7 @@ const VitalsWidget = () => {
             <div className="justify-center flex-1 space-x-4 flex flex-row">
                 {icon}
                 <div className="flex flex-col">
-                    <h1 className="text-center text-lg font-semibold border-b-2">{label}</h1>
+                    <h1 className="text-center text-lg font-semibold border-b-2 border-gray-200">{label}</h1>
                     <p>{value} <span className="italic text-gray-700">{unit}</span></p>
                 </div>
             </div>
@@ -67,7 +67,7 @@ const VitalsWidget = () => {
      */
     const VitalsRow = ({left, right}: {left: VitalsInfo, right?: VitalsInfo}) => {
         return (
-            <li className="py-2 first:pt-0 justify-center divide-x-2 flex flex-row">
+            <li className="py-2 first:pt-0 justify-center divide-x-2 divide-gray-300 flex flex-row">
                 <VitalsCell {...left} />
                 {right ? <VitalsCell {...right}/> : null}
             </li>
@@ -94,7 +94,7 @@ const VitalsWidget = () => {
         <div className="w-full">
             <div className="h-full bg-slate-100 p-2 flex flex-col">
                 <div className="grow">
-                    <ul className="w-full divide-y-2">
+                    <ul className="w-full divide-y-2 divide-gray-300">
                         {vitalsRows}
                     </ul>
                 </div>
