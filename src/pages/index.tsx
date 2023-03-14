bd925239a14b75a36d17bf435154021cc3b25222
Brandon Egger <brandonegger64@gmail.com>
2023-03-14 18:41:15 -0500

    move page title and other header content alongside nav bar to component for easier reuse

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,10 @@ import { type NextPage } from "next";
 import { useSession } from 'next-auth/react';
 import React from 'react';
 
+import MainHeader from '../components/Header';
 import type { HorizontalCarouselSlideProps } from "../components/HorizontalCarousel";
 import { FadedTitleBodyCarouselSlide } from "../components/HorizontalCarousel";
 import HorizontalCarousel from "../components/HorizontalCarousel";
-import NavigationBar from "../components/NavigationBar";
 import indexContent from '../data/index.json';
 
 /**
@@ -28,12 +28,7 @@ const Home: NextPage = () => {
   return (
     <>
       <main className="max-w-[1400px] mx-auto">
-        <div id='header-content' className="m-6">
-          <h2 className="mb-6 text-4xl font-semibold flex items-center space-x-2"><HeartIcon className='w-9 h-9 text-red-600'/>
-            <span>Hospitality</span>
-          </h2>
-          <NavigationBar session={sessionData}/>
-        </div>
+        <MainHeader session={sessionData} />
         <div id='main-content' className="mx-6">
           <HorizontalCarousel slides={carouselSlides} autoCycle={10}/>
         </div>
