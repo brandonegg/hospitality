6618a779ec2a686105a63e50fde4c8d084ae5c74
Brandon Egger <brandonegger64@gmail.com>
2023-03-14 19:13:15 -0500

    fix setup login credentials await page

---
+++ tests/e2e/dashboard.spec.ts
@@ -0,0 +1,25 @@
+import { expect, test } from '@playwright/test';
+
+test.describe('dashboard', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/dashboard');
+    });
+
+    test.describe('logged out', () => {
+        test('redirect to login page', ({page}) => {
+            expect(page.url()).toContain('login');
+        });
+    })
+
+    test.describe('logged in', () => {
+        test.use({
+            storageState: async ({browserName}, use) => {
+                await use(`playwright/.auth/${browserName}/user.json`);
+            },
+        });
+
+        test('on the dashboard page', ({page}) => {
+            expect(page.url()).toContain('login');
+        });
+    })
+});
