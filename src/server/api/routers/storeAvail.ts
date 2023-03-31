336fa6e88ad1d7a721f5b83ed42a51ea6507397f
Blake Hohmann <blake-hohmann@uiowa.edu>
2023-03-31 00:46:58 -0500

    more storeAvail tests

--- src/server/api/routers/storeAvail.ts
+++ src/server/api/routers/storeAvail.ts
@@ -14,6 +14,76 @@ const defaultAvailSelector = Prisma.validator<Prisma.AvailabilitySelect>()({
     date:true,
   });
 
+/**
+ * Process the input to make the correct data to store in the database
+ * @param input 
+ * @returns 
+ */
+export const processInput = (input: {
+    day: number;
+    startTime: string;
+    docId: string;
+    weekCount: number;
+}) => {
+    return {
+        day: input.day,
+        startTime: input.startTime,
+        endTime: makeEndTime(input.startTime),
+        docId: input.docId,
+        date: makeCorrectDate(new Date(), input.day, input.weekCount)
+    }
+}
+
+/**
+     * make end time from start time
+     * @param startTime
+     * @returns 
+     */
+export const makeEndTime = (startTime: string) => {
+    const timeAndAmPM = startTime.split(" ");
+    type amPmType = "am" | "pm";
+    /**
+     * handles 11:30 am to 12:00 pm and 11:30 pm to 12:00 am
+     * @param amPm 
+     * @returns 
+     */
+    function flipAmPm(amPm: amPmType): amPmType {
+        if (amPm === "am") return "pm";
+        else return "am";
+    }
+    let amPm = timeAndAmPM[1] as amPmType;
+    const startHour = parseInt((timeAndAmPM[0] as string).split(":")[0] as string);
+    const startMin = parseInt((timeAndAmPM[0] as string).split(":")[1] as string);
+    let endHour = startHour;
+    let endMin = "30";
+    if (startMin === 30){ // hour has to increase
+        if (startHour === 11) {
+            endHour = 12;
+            amPm = flipAmPm(amPm);
+        }
+        else if (startHour === 12){
+            endHour = 1;
+        }
+        else {endHour += 1;}
+        endMin = "00";
+    }
+    
+    return `${endHour}:${endMin} ${amPm as string}`;
+};
+
+/**
+ * Make the correct Date to store in the database
+ * @param day 
+ * @param weekCount 
+ */
+export const makeCorrectDate = (startDate:Date, day: number, weekCount: number) => {
+        const todayDay = startDate.getDay();
+        const offset = -(todayDay - day) + weekCount * 7;
+        const newDay = new Date(startDate.getTime());
+        newDay.setDate(newDay.getDate() + offset); // properly handles day and increments month when necessary
+        return newDay;
+    }
+
 export const storeAvailRouter = createTRPCRouter({
     storeAvails: publicProcedure
         .input(
@@ -25,62 +95,8 @@ export const storeAvailRouter = createTRPCRouter({
             })
         )
         .mutation(async ({ ctx, input }) => {
-            /**
-             * make end time from start time
-             * @param startTime
-             * @returns 
-             */
-            const makeEndTime = (startTime: string) => {
-                const timeAndAmPM = startTime.split(" ");
-                const startHour = parseInt((timeAndAmPM[0] as string).split(":")[0] as string);
-                const startMin = parseInt((timeAndAmPM[0] as string).split(":")[1] as string);
-                let endHour = startHour;
-                let endMin = "30";
-                if (startMin === 30){ // hour has to increase
-                    if (startHour !== 12) {
-                        endHour = startHour + 1;
-                        timeAndAmPM[1] = "pm";
-                    }
-                    else {endHour = 1;}
-                    endMin = "00";
-                }
-                
-                return `${endHour}:${endMin}`;
-            };
-
-            /**
-             * Make the correct Date to store in the database
-             * @param day 
-             * @param weekCount 
-             */
-            const makeCorrectDate = (day: number, weekCount: number) => {
-                const storeDay = day;
-                const offset = todayDay - (storeDay + weekCount * 7);
-                const newDay = new Date(today.getTime());
-                newDay.setDate(newDay.getDate()-offset); // properly handles day and increments month when necessary
-                return newDay;
-            }
-
-
-            const today = new Date();
-            const todayDay = today.getDay();
-
-            console.log( {
-                day: input.day,
-                startTime: input.startTime,
-                endTime: makeEndTime(input.startTime),
-                docId: input.docId,
-                date: makeCorrectDate(input.day, input.weekCount)
-            });
-
-            const data = {
-                day: input.day,
-                startTime: input.startTime,
-                endTime: makeEndTime(input.startTime),
-                docId: input.docId,
-                date: makeCorrectDate(input.day, input.weekCount)
-            }
-            console.log(data)
+            
+            const data = processInput(input);
 
             const avail = await ctx.prisma.availability.create({
                 data,
@@ -92,7 +108,6 @@ export const storeAvailRouter = createTRPCRouter({
                 select: defaultAvailSelector,
             });
 
-            console.log(avail);
             return avail; // origAvail is same
         })
 });
--- tests/jest/availability.test.ts
+++ tests/jest/availability.test.ts
@@ -1,19 +1,14 @@
 import { describe, expect, test } from "@jest/globals";
-import type { Address, Availability, OriginalAvailability, PrismaClient, User } from "@prisma/client";
-import { type inferProcedureInput, TRPCError } from "@trpc/server";
+import type { Availability, OriginalAvailability, PrismaClient } from "@prisma/client";
+import { type inferProcedureInput } from "@trpc/server";
 import { mockDeep } from "jest-mock-extended";
 
 import { appRouter, type AppRouter } from "../../src/server/api/root";
+import { makeCorrectDate,makeEndTime,processInput } from "../../src/server/api/routers/storeAvail";
 import type { RouterOutputs } from "../../src/utils/api";
 
-const mockAvailabilty: Availability | OriginalAvailability | null = {
-  id: "test-id",
-  day: 1,
-  startTime: "9:00 am",
-  endTime: "9:30 am",
-  docId: "test-doc-id",
-  date: new Date(),
-};
+const today = new Date();
+today.setDate(today.getDate() - (today.getDay()-1));
 
 const emptyAvail : Availability | OriginalAvailability | null = null;
 
@@ -49,46 +44,82 @@ describe("availability router", () => {
 
     const result = await caller.storeAvail.storeAvails(input);
 
-    expect(result).toEqual(mockOutput);
+    expect(result).toStrictEqual(mockOutput);
+  });
+});
+
+describe("makeEndTime", () => {
+  test("basic end time 00 am", () => expect(makeEndTime("9:00 am")).toBe("9:30 am"));
+  test("basic end time 30 am", () => expect(makeEndTime("9:30 am")).toBe("10:00 am"));
+  test("basic end time 00 pm", () => expect(makeEndTime("9:00 pm")).toBe("9:30 pm"));
+  test("basic end time 30 pm", () => expect(makeEndTime("9:30 pm")).toBe("10:00 pm"));
+  test("am to pm", () => expect(makeEndTime("11:30 am")).toBe("12:00 pm"));
+  test("pm to am", () => expect(makeEndTime("11:30 pm")).toBe("12:00 am"));
+  test("end time 11:30 pm", () => expect(makeEndTime("11:30 pm")).toBe("12:00 am"));
+  test("end time 12:00 am", () => expect(makeEndTime("12:00 am")).toBe("12:30 am"));
+  test("end time 12:30 am", () => expect(makeEndTime("12:30 am")).toBe("1:00 am"));
+  test("end time 12:00 pm", () => expect(makeEndTime("12:00 pm")).toBe("12:30 pm"));
+  test("end time 12:30 pm", () => expect(makeEndTime("12:30 pm")).toBe("1:00 pm"));
+});
+
+// https://www.convertunits.com/dates/daysfromdate/ to confirm date times
+describe("makeCorrectDate", () => {
+  describe("no week offset, no day offset", () =>{
+    test("Monday, no week offset, no day offset", () => expect(makeCorrectDate(new Date("March 12, 2023"), 0,0).toDateString()).toStrictEqual(new Date("March 12, 2023").toDateString()));
+    test("Tuesday, no week offset, no day offset", () => expect(makeCorrectDate(new Date("March 13, 2023"), 1,0).toDateString()).toStrictEqual(new Date("March 13, 2023").toDateString()));
+    test("Wednesday, no week offset, no day offset", () => expect(makeCorrectDate(new Date("March 14, 2023"), 2,0).toDateString()).toStrictEqual(new Date("March 14, 2023").toDateString()));
+    test("Thursday, no week offset, no day offset", () => expect(makeCorrectDate(new Date("March 15, 2023"), 3,0).toDateString()).toStrictEqual(new Date("March 15, 2023").toDateString()));
+    test("Friday, no week offset, no day offset", () => expect(makeCorrectDate(new Date("March 16, 2023"), 4,0).toDateString()).toStrictEqual(new Date("March 16, 2023").toDateString()));
+    test("Saturday, no week offset, no day offset", () => expect(makeCorrectDate(new Date("March 17, 2023"), 5,0).toDateString()).toStrictEqual(new Date("March 17, 2023").toDateString()));
+    test("Sunday, no week offset, no day offset", () => expect(makeCorrectDate(new Date("March 18, 2023"), 6,0).toDateString()).toStrictEqual(new Date("March 18, 2023").toDateString()));
   });
 
-  test("store an availability in next week", async () => {
-    // mock the prisma client db
-    const prismaMock = mockDeep<PrismaClient>();
+  describe ("week offset, no day offset", () => {
+    test("1 week offset, no day offset", () => expect(makeCorrectDate(new Date("March 17, 2023"), 5,1).toDateString()).toStrictEqual(new Date("March 24, 2023").toDateString()));
+    test("2 week offset, no day offset", () => expect(makeCorrectDate(new Date("March 17, 2023"), 5,2).toDateString()).toStrictEqual(new Date("March 31, 2023").toDateString()));
+    test("3 week offset, no day offset, new Month", () => expect(makeCorrectDate(new Date("March 17, 2023"), 5,3).toDateString()).toStrictEqual(new Date("April 7, 2023").toDateString()));
+    test("-1 week offset, no day offset", () => expect(makeCorrectDate(new Date("March 17, 2023"), 5,-1).toDateString()).toStrictEqual(new Date("March 10, 2023").toDateString()));
+    test("-2 week offset, no day offset", () => expect(makeCorrectDate(new Date("March 17, 2023"), 5,-2).toDateString()).toStrictEqual(new Date("March 3, 2023").toDateString()));
+    test("-3 week offset, no day offset, new Month", () => expect(makeCorrectDate(new Date("March 17, 2023"), 5,-3).toDateString()).toStrictEqual(new Date("February 24, 2023").toDateString()));
+  });
+  describe("no week offset, day offset", () => {
+    test("no week offset, 1 day offset", () => expect(makeCorrectDate(new Date("March 12, 2023"), 1,0).toDateString()).toStrictEqual(new Date("March 13, 2023").toDateString()));
+    test("no week offset, 2 day offset", () => expect(makeCorrectDate(new Date("March 12, 2023"), 2,0).toDateString()).toStrictEqual(new Date("March 14, 2023").toDateString()));
+    test("no week offset, 3 day offset", () => expect(makeCorrectDate(new Date("March 12, 2023"), 3,0).toDateString()).toStrictEqual(new Date("March 15, 2023").toDateString()));
+    test("no week offset, 4 day offset", () => expect(makeCorrectDate(new Date("March 12, 2023"), 4,0).toDateString()).toStrictEqual(new Date("March 16, 2023").toDateString()));
+    test("no week offset, 5 day offset", () => expect(makeCorrectDate(new Date("March 12, 2023"), 5,0).toDateString()).toStrictEqual(new Date("March 17, 2023").toDateString()));
+    test("no week offset, 6 day offset", () => expect(makeCorrectDate(new Date("March 12, 2023"), 6,0).toDateString()).toStrictEqual(new Date("March 18, 2023").toDateString()));
+  });
+  describe("week offset, day offset", () => {
+    test("1 week offset, 1 day offset", () => expect(makeCorrectDate(new Date("March 12, 2023"), 1,1).toDateString()).toStrictEqual(new Date("March 20, 2023").toDateString()));
+    test("2 week offset, 2 day offset", () => expect(makeCorrectDate(new Date("March 12, 2023"), 2,2).toDateString()).toStrictEqual(new Date("March 28, 2023").toDateString()));
+    test("3 week offset, 3 day offset, new Month", () => expect(makeCorrectDate(new Date("March 12, 2023"), 3,3).toDateString()).toStrictEqual(new Date("April 5, 2023").toDateString()));
+    test("-1 week offset, 1 day offset", () => expect(makeCorrectDate(new Date("March 12, 2023"), 1,-1).toDateString()).toStrictEqual(new Date("March 6, 2023").toDateString()));
+    test("-2 week offset, 2 day offset, new Month", () => expect(makeCorrectDate(new Date("March 12, 2023"), 2,-2).toDateString()).toStrictEqual(new Date("February 28, 2023").toDateString()));
+    test("-3 week offset, 3 day offset, new Month", () => expect(makeCorrectDate(new Date("March 12, 2023"), 3,-3).toDateString()).toStrictEqual(new Date("February 22, 2023").toDateString()));
+  });
+  test("Leap year", () => expect(makeCorrectDate(new Date("March 14, 2024"), 4,-2).toDateString()).toStrictEqual(new Date("February 29, 2024").toDateString()));
 
-    const caller = appRouter.createCaller({
-      session: null,
-      prisma: prismaMock,
-    });
+});
 
-    type Input = inferProcedureInput<AppRouter["storeAvail"]["storeAvails"]>;
-    
-    const input: Input = {  
+describe("processInput", () => {
+  test("Correct storage for no week offset", () => {    
+    const input = {  
       day: 1,
       startTime: "9:00 am",
       docId: "test-doc-id",
-      weekCount: 1,
+      weekCount: 0,
     };
-
-    const today = new Date();
-    today.setDate(today.getDate() + 7)
-
-    const mockOutput: RouterOutputs["storeAvail"]["storeAvails"] = {
+    const expectedOutput = {
       day: 1,
       startTime: "9:00 am",
-      endTime: "10:30 am",
+      endTime: "9:30 am",
       docId: "test-doc-id",
-      date: today,
+      date: today.toDateString(),
     };
+    const result = processInput(input);
 
-    prismaMock.availability.findFirst.mockResolvedValue(emptyAvail);
-    prismaMock.availability.create.mockResolvedValue(mockOutput as Availability);
-
-    const result = await caller.storeAvail.storeAvails(input);
-    console.log(result);
-    console.log(mockOutput);
-    expect(result).toMatchObject(mockOutput);
+    result.date = result.date.toDateString() as unknown as Date; // get rid of hours mins etc. for comparing
+    expect(result).toStrictEqual(expectedOutput);
   });
-
-
 });
