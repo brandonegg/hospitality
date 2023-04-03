770143dfa29279108348db7c57bd90fe28a4d671
Brandon Egger <brandonegger64@gmail.com>
2023-04-03 10:41:08 -0500

    added column labels for bed table

--- src/pages/beds/index.tsx
+++ src/pages/beds/index.tsx
@@ -5,6 +5,33 @@ import { getSession, useSession } from "next-auth/react";
 import MainHeader from "../../components/Header";
 
 /**
+ * Main beds table element.
+ */
+const BedsTable = () => {
+    return (
+        <div className="flex flex-col">
+            <div className="font-bold bg-neutral-200 text-xl text-center py-2">
+                <h1>Beds</h1>
+            </div>
+            <table className="w-full table-fixed text-left text-sm text-gray-500 dark:text-gray-400">
+                <thead className="bg-slate-800 text-sm uppercase text-gray-300">
+                    <tr>
+                        <th className="min-w-[200px] px-6 py-3 lg:w-1/3">Building</th>
+                        <th className="min-w-[200px] px-6 py-3 lg:w-1/4">Room</th>
+                        <th className="min-w-[175px] px-6 py-3">Status</th>
+                        <th className="max-w-[200px] min-w-[100px] px-6 py-3">Last Occupied</th>
+                        <th className="max-w-[200px] min-w-[100px] px-6 py-3">Last Unoccupied</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+/**
  * Beds Table view for creating, modifying, and deleting beds
  * @returns
  */
@@ -14,6 +41,9 @@ const BedsPage = () => {
     return(
         <main className="mx-auto max-w-[1400px]">
             <MainHeader user={sessionData?.user} />
+            <div className="m-6 overflow-x-auto rounded-xl border border-gray-600 drop-shadow-lg">
+                <BedsTable />
+            </div>
         </main>
     );
 };
