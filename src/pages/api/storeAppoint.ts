f355d4563146c30404be747dbace2ebec8a83915
Blake Hohmann <blake-hohmann@uiowa.edu>
2023-04-03 21:49:00 -0500

    adding more testing, storing and removing avail testing

--- src/pages/api/storeAppoint.ts
+++ src/pages/api/storeAppoint.ts
@@ -1,8 +1,61 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+import { makeCorrectDate } from '../../server/api/routers/storeAvail';
 import {makeEndTime}  from "../../server/api/routers/storeAvail";
 import { prisma } from "../../server/db";
+
+/**
+ * helper for storeAppoints to help with testing  
+ * @param times startTime,day,docId,weekCount,userId
+ */
+export function storeTestHelper(times:[string,number,string,number,string][], overrideAllDoctorId:string | undefined = undefined){
+  const startEndDayInfo:[string,string,number,string,string,number][] = [];
+  times.forEach((timeIntInt) => {
+    const startTime = timeIntInt[0];
+    const day = timeIntInt[1];
+    const docId = timeIntInt[2];
+    const weekCount = timeIntInt[3]; 
+    const userId = timeIntInt[4];
+    const endTime = makeEndTime(startTime);
+    startEndDayInfo.push([startTime,endTime,day,docId,userId,weekCount]); //push in start time
+  });
+  const results:object[] = [];
+  for (const startEndDayArray of startEndDayInfo){
+    const storeDay = startEndDayArray[2];
+    const weekCount = startEndDayArray[5];
+    const newDay = makeCorrectDate(new Date((new Date()).toDateString()), storeDay, weekCount);
+    let doctorId = startEndDayArray[3];
+    
+    let twoDigMonth = (newDay.getMonth() + 1).toString();
+    if (twoDigMonth.length == 1) twoDigMonth = "0" + twoDigMonth;
+    let twoDigDay = newDay.getDate().toString();
+    if (twoDigDay.length == 1) twoDigDay = "0" + twoDigDay;
+
+
+    //const newDayString = `${newDay.getFullYear()}-${twoDigMonth}-${twoDigDay}`;
+    if (doctorId === "AllDoctors"){ // find the first doctor and set this doctor id for the appointment to be made
+      //const tempDoctorId: {docId: string;}[] = await prisma.$queryRawUnsafe(`SELECT docId FROM Availability WHERE day=${storeDay} AND startTime="${startEndDayArray[0]}" AND date="${newDayString}" LIMIT 1`)
+      //if (tempDoctorId[0]) doctorId = tempDoctorId[0].docId;
+      if (overrideAllDoctorId) doctorId = overrideAllDoctorId;
+    }
+    //const removeAvailability = await prisma.$executeRawUnsafe(`DELETE FROM Availability WHERE day=${storeDay} AND startTime="${startEndDayArray[0]}" AND docId="${doctorId}" AND date="${newDayString}"`)
+    results.push({day:storeDay,startTime: startEndDayArray[0],endTime: startEndDayArray[1],docId: doctorId,userId: startEndDayArray[4],date: newDay})
+    // const result = await prisma.appointment.create({
+    //   data: {
+    //     day:storeDay, 
+    //     startTime: startEndDayArray[0],
+    //     endTime: startEndDayArray[1],
+    //     docId: doctorId,
+    //     userId: startEndDayArray[4],
+    //     date: newDay, // later get the correct date
+    //   },
+    // })
+    // results.push(result);
+    // results.push(removeAvailability);
+    return results
+  }
+}
 
 /**
  * add times to db
@@ -27,28 +80,17 @@ export default async function storeAppoints(
   });
   const results = [];
   // avoid time zone shenanigans by stripping the seconds and hours etc.
-  const today = new Date((new Date()).toDateString())
-  const todayDay = today.getDay();
   for await (const startEndDayArray of startEndDayInfo){
     const storeDay = startEndDayArray[2];
     const weekCount = startEndDayArray[5];
-    const offset = todayDay - (storeDay + weekCount * 7);
-    const newDay = new Date(today.getTime());
-    newDay.setDate(newDay.getDate()-offset); // properly handles day and increments month when necessary
+    const newDay = makeCorrectDate(new Date((new Date()).toDateString()), storeDay, weekCount);
     let doctorId = startEndDayArray[3];
-    
-    let twoDigMonth = (newDay.getMonth() + 1).toString();
-    if (twoDigMonth.length == 1) twoDigMonth = "0" + twoDigMonth;
-    let twoDigDay = newDay.getDate().toString();
-    if (twoDigDay.length == 1) twoDigDay = "0" + twoDigDay;
 
-
-    const newDayString = `${newDay.getFullYear()}-${twoDigMonth}-${twoDigDay}`;
     if (doctorId === "AllDoctors"){ // find the first doctor and set this doctor id for the appointment to be made
-      const tempDoctorId: {docId: string;}[] = await prisma.$queryRawUnsafe(`SELECT docId FROM Availability WHERE day=${storeDay} AND startTime="${startEndDayArray[0]}" AND date="${newDayString}" LIMIT 1`)
+      const tempDoctorId: {docId: string;}[] = await prisma.$queryRawUnsafe(`SELECT docId FROM Availability WHERE day=${storeDay} AND startTime="${startEndDayArray[0]}" AND date="${newDay.toDateString()}" LIMIT 1`)
       if (tempDoctorId[0]) doctorId = tempDoctorId[0].docId;
     }
-    const removeAvailability = await prisma.$executeRawUnsafe(`DELETE FROM Availability WHERE day=${storeDay} AND startTime="${startEndDayArray[0]}" AND docId="${doctorId}" AND date="${newDayString}"`)
+    const removeAvailability = await prisma.$executeRawUnsafe(`DELETE FROM Availability WHERE day=${storeDay} AND startTime="${startEndDayArray[0]}" AND docId="${doctorId}" AND date="${newDay.toDateString()}"`)
 
     const result = await prisma.appointment.create({
       data: {
--- tests/jest/appointment.test.ts
+++ tests/jest/appointment.test.ts
@@ -2,9 +2,9 @@ import { describe, test } from "@jest/globals";
 import type { Appointment, PrismaClient } from "@prisma/client";
 import { mockDeep } from "jest-mock-extended";
 
+import { storeTestHelper } from "../../src/pages/api/storeAppoint";
 import { appRouter } from "../../src/server/api/root";
-import { prisma } from "../../src/server/db";
-import { api } from "../../src/utils/api";
+import { makeCorrectDate } from "../../src/server/api/routers/storeAvail";
 
 const mockAppointment: Appointment = {
   id: "test-id",
@@ -42,12 +42,141 @@ describe("appointment", () => {
   });
 });
 
-// describe("test making an appointment removing availability time", () => {
-//   test("appointment made", () => {
+describe("test making an appointment removing availability time", () => {
+  test("does the appointment object that is removed match the avail that is created when docId is not all doc", () => {
+    // startTime,day,docId,weekCount,userId
+    const times:[string,number,string,number,string][] = [["9:30 am", 1, "test-doc-id", 0, "testUser2"]];
+    const results = storeTestHelper(times);
 
-    
-//     const { mutate }= api.storeAvail.storeAvails.useMutation();
-//     console.log(mutate({day: 1, startTime: "9:30 am", docId: "test-doc-id", weekCount:0}))
+    const newDay = makeCorrectDate(new Date((new Date()).toDateString()), 1, 0);
 
-//   });
-// });
+    const expectedOutput = {
+      day:1, 
+      startTime: "9:30 am",
+      endTime: "10:00 am",
+      docId: "test-doc-id",
+      userId: "testUser2",
+      date: newDay, // later get the correct date
+    };
+    expect((results as object[])[0]).toEqual(expectedOutput);
+  });
+  test("does the appointment object that is removed match the avail that is created when docId is all doc", () => {
+    // startTime,day,docId,weekCount,userId
+    const times:[string,number,string,number,string][] = [["9:30 am", 1, "AllDoctors", 0, "testUser2"]];
+    const results = storeTestHelper(times, "actualDoctorIdNotAllDoctors");
+
+    const newDay = makeCorrectDate(new Date((new Date()).toDateString()), 1, 0);
+
+    const expectedOutput = {
+      day:1, 
+      startTime: "9:30 am",
+      endTime: "10:00 am",
+      docId: "actualDoctorIdNotAllDoctors",
+      userId: "testUser2",
+      date: newDay, // later get the correct date
+    };
+    expect((results as object[])[0]).toEqual(expectedOutput);
+  });
+  test("new startTime", () => {
+    // startTime,day,docId,weekCount,userId
+    const times:[string,number,string,number,string][] = [["10:00 am", 1, "test-doc-id", 0, "testUser2"]];
+    const results = storeTestHelper(times);
+
+    const newDay = makeCorrectDate(new Date((new Date()).toDateString()), 1, 0);
+
+    const expectedOutput = {
+      day:1, 
+      startTime: "10:00 am",
+      endTime: "10:30 am",
+      docId: "test-doc-id",
+      userId: "testUser2",
+      date: newDay, // later get the correct date
+    };
+    expect((results as object[])[0]).toEqual(expectedOutput);
+  });
+  test("new startTime", () => {
+    // startTime,day,docId,weekCount,userId
+    const times:[string,number,string,number,string][] = [["10:30 am", 1, "test-doc-id", 0, "testUser2"]];
+    const results = storeTestHelper(times);
+
+    const newDay = makeCorrectDate(new Date((new Date()).toDateString()), 1, 0);
+
+    const expectedOutput = {
+      day:1, 
+      startTime: "10:30 am",
+      endTime: "11:00 am",
+      docId: "test-doc-id",
+      userId: "testUser2",
+      date: newDay, // later get the correct date
+    };
+    expect((results as object[])[0]).toEqual(expectedOutput);
+  });
+  test("new startTime", () => {
+    // startTime,day,docId,weekCount,userId
+    const times:[string,number,string,number,string][] = [["11:00 am", 1, "test-doc-id", 0, "testUser2"]];
+    const results = storeTestHelper(times);
+
+    const newDay = makeCorrectDate(new Date((new Date()).toDateString()), 1, 0);
+
+    const expectedOutput = {
+      day:1, 
+      startTime: "11:00 am",
+      endTime: "11:30 am",
+      docId: "test-doc-id",
+      userId: "testUser2",
+      date: newDay, // later get the correct date
+    };
+    expect((results as object[])[0]).toEqual(expectedOutput);
+  });
+  test("new startTime am to pm", () => {
+    // startTime,day,docId,weekCount,userId
+    const times:[string,number,string,number,string][] = [["11:30 am", 1, "test-doc-id", 0, "testUser2"]];
+    const results = storeTestHelper(times);
+
+    const newDay = makeCorrectDate(new Date((new Date()).toDateString()), 1, 0);
+
+    const expectedOutput = {
+      day:1, 
+      startTime: "11:30 am",
+      endTime: "12:00 pm",
+      docId: "test-doc-id",
+      userId: "testUser2",
+      date: newDay, // later get the correct date
+    };
+    expect((results as object[])[0]).toEqual(expectedOutput);
+  });
+  test("new startTime pm to am", () => {
+    // startTime,day,docId,weekCount,userId
+    const times:[string,number,string,number,string][] = [["11:30 pm", 1, "test-doc-id", 0, "testUser2"]];
+    const results = storeTestHelper(times);
+
+    const newDay = makeCorrectDate(new Date((new Date()).toDateString()), 1, 0);
+
+    const expectedOutput = {
+      day:1, 
+      startTime: "11:30 pm",
+      endTime: "12:00 am",
+      docId: "test-doc-id",
+      userId: "testUser2",
+      date: newDay, // later get the correct date
+    };
+    expect((results as object[])[0]).toEqual(expectedOutput);
+  });
+  test("AllDoctors id doesn't save AllDoctors", () => {
+    // startTime,day,docId,weekCount,userId
+    const times:[string,number,string,number,string][] = [["11:30 pm", 1, "AllDoctors", 0, "testUser2"]];
+    const results = storeTestHelper(times, "testDocId");
+
+    const newDay = makeCorrectDate(new Date((new Date()).toDateString()), 1, 0);
+
+    const expectedOutput = {
+      day:1, 
+      startTime: "11:30 pm",
+      endTime: "12:00 am",
+      docId: "AllDoctors",
+      userId: "testUser2",
+      date: newDay, // later get the correct date
+    };
+    expect((results as object[])[0]).not.toEqual(expectedOutput);
+  });
+});
