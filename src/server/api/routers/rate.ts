0aebfe6a12826bac01cffbd6b76e21f3354f5fcd
Kevin Dao <kevin-dao@uiowa.edu>
2023-04-12 10:28:17 -0500

    Add getall rate server route

---
+++ src/server/api/routers/rate.ts
@@ -0,0 +1,35 @@
+import { z } from "zod";
+
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+
+export const rateRouter = createTRPCRouter({
+  getAll: protectedProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100).default(10),
+        cursor: z.string().nullish(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { limit, cursor } = input;
+      const [count, items] = await ctx.prisma.$transaction([
+        ctx.prisma.rate.count(),
+        ctx.prisma.rate.findMany({
+          take: limit + 1,
+          cursor: cursor ? { id: cursor } : undefined,
+        }),
+      ]);
+
+      let nextCursor: typeof cursor | undefined = undefined;
+      if (items.length > limit) {
+        const nextItem = items.pop(); // return the last item from the array
+        nextCursor = nextItem?.id;
+      }
+
+      return {
+        count,
+        items,
+        nextCursor,
+      };
+    }),
+});
