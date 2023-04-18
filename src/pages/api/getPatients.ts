14414e3ef7e239a9f7b3b55325d99fb09b5c8008
Blake Hohmann <blake-hohmann@uiowa.edu>
2023-04-18 00:09:25 -0500

    can add to bill

--- src/pages/api/getPatients.ts
+++ src/pages/api/getPatients.ts
@@ -1,17 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { User } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 import { prisma } from "../../server/db";
 
 /**
- * get a given doctors availability for appointments
+ * get all patients who don't have an invoice
  */
 export default async function getPatients(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const result = await prisma.$queryRawUnsafe(
+  const result: User[] = await prisma.$queryRawUnsafe(
     `SELECT * FROM User WHERE role="PATIENT"`
   );
-  return res.status(201).json(result);
+  const invoices: { userId: string }[] = await prisma.$queryRawUnsafe(
+    `SELECT userId FROM Invoice`
+  );
+  const invoiceUserIds = invoices.map((invoice) => invoice.userId);
+
+  const filterFun = function (user: User) {
+    return !invoiceUserIds.includes(user.id); //use the argument here.
+  };
+  const fin = result.filter(filterFun);
+  return res.status(201).json(fin);
 }
