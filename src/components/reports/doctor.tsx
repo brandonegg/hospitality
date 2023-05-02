83063bf0efd6bf645a048f5bc425832df36dca80
Brandon Egger <brandonegger64@gmail.com>
2023-05-02 17:25:02 -0500

    complete visit report line items view

--- src/components/reports/doctor.tsx
+++ src/components/reports/doctor.tsx
@@ -1,6 +1,7 @@
 import { TrashIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
+import { dateToString } from "../../lib/text";
 import type { VisitReportSummary } from "../../server/api/routers/visitReport";
 
 /**
@@ -9,7 +10,7 @@ import type { VisitReportSummary } from "../../server/api/routers/visitReport";
 const CreateReportButton = () => {
   return (
     <Link
-      className="rounded-lg border border-blue-400 bg-blue-500 px-4 py-2 font-semibold text-white hover:bg-blue-600"
+      className="rounded-lg border border-blue-400 bg-blue-500 px-4 py-2 font-semibold text-white drop-shadow-lg transition duration-200 hover:bg-blue-600 hover:drop-shadow-sm"
       href="/report/create"
     >
       Create Report +
@@ -28,12 +29,27 @@ const ReportLine = ({ report }: { report: VisitReportSummary }) => {
 
   return (
     <div className="flex flex-row justify-between rounded-xl border border-neutral-200 bg-neutral-100 py-2 px-4 shadow-lg">
-      <div className="grid h-16 w-16 place-items-center rounded-full border border border-neutral-400 bg-green-300">
-        <span className="text-2xl font-bold">{shortName}</span>
+      <div className="flex flex-row space-x-4">
+        <div className="grid h-16 w-16 place-items-center rounded-full border border border-neutral-400 bg-green-300">
+          <span className="text-2xl font-bold">{shortName}</span>
+        </div>
+        <div className="my-auto h-fit space-x-2">
+          <h1 className="inline-block text-xl font-semibold">
+            {report.patient_name}
+          </h1>
+          <span className="inline-block text-lg italic text-neutral-400">
+            on {dateToString(report.date)}
+          </span>
+        </div>
       </div>
-      <button className="group my-auto h-fit rounded-lg border border-red-300 bg-red-200 p-2 hover:bg-red-400">
-        <TrashIcon className="w-8 text-neutral-600 group-hover:text-black" />
-      </button>
+      <div className="my-auto flex h-fit flex-row space-x-4">
+        <button className="group my-auto grid h-12 place-items-center rounded-lg border border-blue-300 bg-blue-200 px-4 font-semibold text-neutral-600 hover:bg-blue-400 hover:text-white">
+          View Report
+        </button>
+        <button className="group my-auto grid h-12 w-12 place-items-center rounded-lg border border-red-300 bg-red-200 hover:bg-red-400">
+          <TrashIcon className="w-8 text-neutral-600 group-hover:text-black" />
+        </button>
+      </div>
     </div>
   );
 };
@@ -48,10 +64,10 @@ const DoctorReportDashboard = ({
 }) => {
   return (
     <div className="mx-auto max-w-2xl">
-      <div className="flex flex-row justify-end rounded-xl border border-neutral-400 bg-neutral-200 p-2 drop-shadow">
+      <div className="mx-auto w-fit">
         <CreateReportButton />
       </div>
-      <div className="space-y-4 p-4">
+      <div className="space-y-4 py-4 px-8">
         {!reports || reports.length == 0 ? (
           <h1 className="text-center text-xl font-bold text-neutral-600">
             No reports authored yet!
@@ -62,7 +78,7 @@ const DoctorReportDashboard = ({
           </h1>
         )}
         {/** Display reports created by doctor */}
-        <div className="px-4">
+        <div className="">
           {reports?.map((report, index) => {
             return <ReportLine key={index} report={report} />;
           })}
